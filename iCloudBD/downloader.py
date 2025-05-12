4a8d5e2b138aaf360e18e51927887be4691d217b
Aarni Koskela <akx@iki.fi>
2025-05-12 11:19:15 +0300

    Ruff format

--- iCloudBD/downloader.py
+++ iCloudBD/downloader.py
@@ -17,21 +17,24 @@ def download_item(item, sess=None):
 
     os.makedirs(os.path.dirname(item.file_name), exist_ok=True)
     if os.path.exists(item.file_name):
-        print('Already exists: %s' % item.file_name)
+        print("Already exists: %s" % item.file_name)
         return False
 
-    print('Downloading photo %s derivative %s to %s (%s bytes)' % (
-        item.template_namespace['photo_guid'],
-        item.template_namespace['derivative_id'],
-        item.file_name,
-        item.derivative['fileSize'],
-    ))
+    print(
+        "Downloading photo %s derivative %s to %s (%s bytes)"
+        % (
+            item.template_namespace["photo_guid"],
+            item.template_namespace["derivative_id"],
+            item.file_name,
+            item.derivative["fileSize"],
+        )
+    )
     r = sess.get(item.url, stream=True)
     r.raise_for_status()
 
-    temp_name = item.file_name + '.tmp-%s' % time.time()
+    temp_name = item.file_name + ".tmp-%s" % time.time()
     try:
-        with open(temp_name, 'wb') as f:
+        with open(temp_name, "wb") as f:
             for chunk in r.iter_content(chunk_size=512 * 1024):
                 if chunk:
                     f.write(chunk)
@@ -48,7 +51,10 @@ def download_item(item, sess=None):
 def perform_download(download_items, parallel=0):
     if parallel > 1:
         import multiprocessing
-        with multiprocessing.Pool(processes=parallel, initializer=subprocess_initializer) as p:
+
+        with multiprocessing.Pool(
+            processes=parallel, initializer=subprocess_initializer
+        ) as p:
             for result in p.imap_unordered(download_item, download_items, chunksize=10):
                 pass
     else:
